e7bb3b2eb58f8aacec72fd79bd289638ebc0f4cd
Henockb3 <52932865+Henockb3@users.noreply.github.com>
2022-01-04 21:21:31 -0800

    Update agent.ts

--- CrptoAppAngular/src/app/api/agent.ts
+++ CrptoAppAngular/src/app/api/agent.ts
@@ -10,7 +10,7 @@ import { Crypto } from "../model/crypto";
     providedIn: 'root'
 })
 export class Agent {
-    url = 'https://thawing-meadow-69111.herokuapp.com';
+    url = 'https://cryptoappapiforangular.herokuapp.com';
 
     public userFromAPI: Promise<User>;
     public portfolioFromAPI: Promise<Portfolio>;
